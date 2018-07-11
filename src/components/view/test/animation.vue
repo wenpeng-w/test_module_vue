b5e4737cf188f42bebdfd565b97409ea8e90ebc7
wenpeng-w <449202631@qq.com>
2018-07-11 11:23:30 +0800

    add vue animation module

--- src/components/view/test/animation.vue
+++ src/components/view/test/animation.vue
@@ -10,6 +10,12 @@
     <transition name="enter-right">
       <div v-if="showRight" class="animation-text ani-right"></div>
     </transition>
+    <transition name="enter-top">
+      <div v-if="showTop" class="animation-text ani-top"></div>
+    </transition>
+    <transition name="enter-bottom">
+      <div v-if="showBottom" class="animation-text ani-bottom"></div>
+    </transition>
   </div>
 </template>
 
@@ -53,15 +59,23 @@
     width: 50%;
     height: 2rem;
     position: fixed;
-    top: 1rem;
   }
-  .ani-left { left: 2%; }
-  .ani-right { right: 2%; }
-
+  .ani-left { top: 1rem; left: 2%; }
+  .ani-right { top: 1rem; right: 2%; }
+  .ani-top {
+    top: 50%;
+    left: 50%;
+    transform: translate3d(-50%, -50%, 0);
+  }
+  .ani-bottom {
+    bottom: 50%;
+    left: 50%;
+    transform: translate3d(-50%, -50%, 0);
+  }
   /**
    * 可以不设置 enter-right-enter-to 和 enter-right-leave 类
-   * enter-right-enter-to 进入过度的结束状态
-   * enter-right-leave 离开过度的开始状态
+   * *-enter-to 进入过度的结束状态
+   * *-leave 离开过度的开始状态
    **/
 
   // 左边进入
@@ -73,4 +87,14 @@
   .enter-right-enter-active, .enter-right-leave-active { transition: all .6s ease-in-out; }
   .enter-right-enter-to, .enter-right-leave { right: 2%; opacity: 1; }
   .enter-right-enter, .enter-right-leave-to { right: -100%; opacity: 0; }
+
+  // 顶部进入
+  .enter-top-enter-active, .enter-top-leave-active { transition: all .6s ease-in-out; }
+  .enter-top-enter-to, .enter-top-leave { top: 50%; opacity: 1; }
+  .enter-top-enter, .enter-top-leave-to { top: -100%; opacity: 0; }
+
+  // 底部进入
+  .enter-bottom-enter-active, .enter-bottom-leave-active { transition: all .6s ease-in-out; }
+  .enter-bottom-enter-to, .enter-bottom-leave { bottom: 50%; opacity: 1; }
+  .enter-bottom-enter, .enter-bottom-leave-to { bottom: -100%; opacity: 0; }
 </style>
