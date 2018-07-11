edfe673a239c974fb397a1b0a41ea620d6de0301
wenpeng-w <449202631@qq.com>
2018-07-11 18:03:19 +0800

    svg path

--- src/components/view/test/SVG.vue
+++ src/components/view/test/SVG.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="SVG-container">
-    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100px" height="100px">
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100px">
       <g>
         <polygon class="color-hover" points="23,32,53,32,42,14,43,53"></polygon>
       </g>
+    </svg>
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100px">
+      <path fill="transparent" stroke="black" d="M 0,100 Q 50,10 180,70 T 380,50, 700,90"></path>
+      <path fill="transparent" stroke="black" d="M 0,60 Q 100,100 180,70 T 380,50, 700,90"></path>
     </svg>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'svg',
+    name: 'svgs',
     data () {
       return {
         svgPath: []
@@ -21,6 +25,9 @@
 </script>
 
 <style lang="less" scoped>
+  .SVG-container {
+    margin: 0 -0.24rem;
+  }
   .color-hover {
     fill: #fff;
     transition: fill .3s ease-in-out;
