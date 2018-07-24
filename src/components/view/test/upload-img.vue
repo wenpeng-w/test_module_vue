474269ea3abf9049c55f19dff35fdeac49b2edd9
wenpeng-w <449202631@qq.com>
2018-07-24 15:27:46 +0800

    upload file

--- src/components/view/test/upload-img.vue
+++ src/components/view/test/upload-img.vue
@@ -3,7 +3,11 @@
     <!--上传图片-->
     <div class="upload-image">
       <van-button type="primary" size="small">upload image</van-button>
-      <input class="file-hide" @change="upload" multiple type="file" accept="image/*">
+      <input class="file-hide" @change="uploadImage" multiple type="file" accept="image/*">
+    </div>
+    <div class="upload-image">
+      <van-button type="primary" size="small">upload text</van-button>
+      <input class="file-hide" @change="uploadText" type="file" accept="text/*">
     </div>
     <div v-if="pics.length > 0" class="img-container">
       <div v-for="(pic, index) in pics" :key="index" class="img-item"><img :src="pic" :alt="`pic_${index}`"></div>
@@ -26,22 +30,25 @@ export default {
   methods: {
     ...mapMutations(['SET_HEADER']),
     setHeader () {
-      this.SET_HEADER('上传图片');
+      this.SET_HEADER('上传文件');
     },
 
     /**
      * 上传图片
      * @param e 包含 file 对象的 fileList 对象
      * 方法1. window.URL.createObjectURL()
-     * 方法2. FileReader()
+     * 方法2. FileReader() 读取文件
      */
-    upload (e) {
+    uploadImage (e) {
+      console.log(e);
       let files = e.target.files;
       if (!files || files.length === 0) return;
       files = Array.from(files);
       files.forEach(file => {
         // 方法1
-        this.pics.push(window.URL.createObjectURL(file));
+        let pic = window.URL.createObjectURL(file);
+        this.pics.push(pic);
+        // window.URL.revokeObjectURL(pic);
 
         // 方法2
         // let reader = new FileReader();
@@ -51,6 +58,20 @@ export default {
         // };
         // reader.readAsDataURL(file);
       });
+    },
+    /**
+     * 上传 TEXT 文件
+     * @param e
+     */
+    uploadText (e) {
+      console.log(e);
+      let files = e.target.files[0];
+      if (!files || files.length === 0) return;
+      let reader = new FileReader();
+      reader.onloadend = () => {
+        this.pics.push(reader.result);
+      };
+      reader.readAsText(files, 'gb2312');
     }
   }
 };
